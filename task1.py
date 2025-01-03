01db7ef1d290fda6c60a102875b0a60d9d4dd3e1
Docvoids <rgordienko872@gmail.com>
2025-01-03 19:56:14 +0200

    Implement async_filter

--- task1.py
+++ task1.py
@@ -10,3 +10,15 @@ async def async_map(
 ) -> List[U]:
     tasks = [fn(item) for item in data]
     return await asyncio.gather(*tasks)
+
+async def async_filter(
+    predicate: Callable[[T], Coroutine[Any, Any, bool]],
+    data: List[T]
+) -> List[T]:
+    async def check(item):
+        if await predicate(item):
+            return item
+        return None
+    tasks = [check(item) for item in data]
+    results = await asyncio.gather(*tasks)
+    return [item for item in results if item is not None]
