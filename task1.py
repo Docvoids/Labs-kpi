cf685b4b96c73651e17e51c81e30c424d578518c
Docvoids <rgordienko872@gmail.com>
2025-01-03 19:57:07 +0200

    Implement async_some

--- task1.py
+++ task1.py
@@ -30,3 +30,18 @@ async def async_filter_map(
     tasks = [fn(item) for item in data]
     results = await asyncio.gather(*tasks)
     return [item for item in results if item is not None]
+
+async def async_some(
+    predicate: Callable[[T], Coroutine[Any, Any, bool]],
+    data: List[T]
+) -> bool:
+    async def check(item):
+        if await predicate(item):
+            raise StopAsyncIteration()
+        return False
+    tasks = [check(item) for item in data]
+    try:
+        await asyncio.gather(*tasks)
+        return False
+    except StopAsyncIteration:
+        return True
