976808e8fb645c8c31e2f41c2e6b0b88185c3062
Docvoids <rgordienko872@gmail.com>
2025-01-03 20:41:03 +0200

    Add basic Entity class

--- task5.py
+++ task5.py
@@ -13,3 +13,15 @@ class MessageBus:
         if message_type in self._subscribers:
             for subscriber in self._subscribers[message_type]:
                 await subscriber(message_data)
+
+class Entity:
+    def __init__(self, name, message_bus):
+        self.name = name
+        self.message_bus = message_bus
+
+    async def publish_message(self, message_type, data):
+        print(f"{self.name}: Publishing message of type '{message_type}' with data: {data}")
+        await self.message_bus.publish(message_type, data)
+
+    async def handle_message(self, data):
+        print(f"{self.name}: Received message: {data}")
